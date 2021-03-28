1506a183d28aa5b3e3bc37355306a0c3b7a0fad3
Lê Bảo Anh <60655387+b1506704@users.noreply.github.com>
2021-03-29 01:55:57 +0700

    fix

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use('/users', userRoutes);
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
