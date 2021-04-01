3ec394f4d22803c65117365db959a28c382bb095
Lê Bảo Anh <60655387+b1506704@users.noreply.github.com>
2021-04-01 21:59:23 +0700

    switch

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 // url để gửi dữ liệu trên localhost server
-// const userUrl = 'http://localhost:80/users';
-// const accountUrl = 'http://localhost:80/accounts';
-// const categoryUrl = 'http://localhost:80/categories';
+const userUrl = 'http://localhost:80/users';
+const accountUrl = 'http://localhost:80/accounts';
+const categoryUrl = 'http://localhost:80/categories';
 
 // url để gửi dữ liệu đến web trên herokuapp
-const userUrl = 'https://gaming-account-shop.herokuapp.com/users';
-const accountUrl = 'https://gaming-account-shop.herokuapp.com/accounts';
-const categoryUrl = 'https://gaming-account-shop.herokuapp.com/categories';
+// const userUrl = 'https://gaming-account-shop.herokuapp.com/users';
+// const accountUrl = 'https://gaming-account-shop.herokuapp.com/accounts';
+// const categoryUrl = 'https://gaming-account-shop.herokuapp.com/categories';
 
 export const createUser = (newUser) => axios.post(userUrl, newUser);
 export const login = (userInfo) => axios.post(`${userUrl}/${userInfo}`, userInfo);
